f59afdb790fe584083f4e4dd4fd634ec8d928537
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 02:26:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,12 +102,10 @@ class TicTacToe
   def play
     until over? == true
       turn
-      if draw == true
-        puts "Cat's game!"
-      elsif won? !=false
-        if winner == "X"
+      if won? !=false && winner == "X"
           puts "Congratulations, X!"
-        else puts "Congratulations, O!"
+      elsif puts "Congratulations, O!"
+        
         end
       end
     end
